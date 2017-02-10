1fef3b9f1fd4020c5b195413de4cb4809bc7c1be
jcaubin <jcaubin@gmail.com>
2017-02-10 14:45:09 +0100

    estractura de codigo segun tipo de justificante

--- ClienteJustificacion/src/Program.cs
+++ ClienteJustificacion/src/Program.cs
@@ -21,61 +21,23 @@ namespace ClienteJustificacion
             try
             {
                 var clientArgs = Args.Parse<ClientArgs>(args);
-                var parseErrors = new List<ParseResult>(); //Listado de errores de parseo
-
                 Console.WriteLine("Proceso del fichero: {0}; {1}; {2}; {3};", clientArgs.User, "*****", clientArgs.FileType, clientArgs.File);
                 _loger.Info("Proceso del fichero: {0}; {1}; {2}; {3};", clientArgs.User, "*****", clientArgs.FileType, clientArgs.File);
 
-                //Carga y parseo del fichero de datos
-                var csv = new CsvReader(new StreamReader(clientArgs.File));
-                csv.Configuration.Delimiter = ";";
-                csv.Configuration.RegisterClassMap<JbsInterchageModelCsvDocMap>();
-                csv.Configuration.IgnoreReadingExceptions = true;
-                csv.Configuration.ReadingExceptionCallback = (ex, row) =>
+                switch (clientArgs.FileType)
                 {
-                    int rownumber = row.Row;
-                    string message = ex.Message + " " + ex.Data["CsvHelper"];
-                    parseErrors.Add(new ParseResult() { Id = rownumber, Descripcion = message });
-                };
-                var records = csv.GetRecords<JbsInterchageModel>().ToList();
-
-                if (parseErrors.Count > 0)
-                {
-                    Console.WriteLine("Se ha encontrado errores en el fichero de entrada, corrijalos antes de continuar.");
-                    _loger.Error("Se ha encontrado errores en el fichero de entrada, corrijalos antes de continuar.");
-                    foreach (var error in parseErrors)
-                    {
-                        Console.WriteLine("Linea: {0}; Error {1};", error.Id, error.Descripcion);
-                        _loger.Error("Linea: {0}; Error {1};", error.Id, error.Descripcion);
-                    }
-                    return;
-                }
-
-                using (JustificationClient client = new JustificationClient())
-                {
-                    client.ClientCredentials.UserName.UserName = clientArgs.User;
-                    client.ClientCredentials.UserName.Password = clientArgs.Password;
-                    var resultadosEnvio = new List<LoadResult>();
-                    foreach (var item in records)
-                    {
-                        Console.Write("Envio: {0}, {1}.", item.Expediente, item.NumeroFactura);
-                        if (!string.IsNullOrWhiteSpace(item.NombreFichero))
-                        {
-                            item.Fichero = File.ReadAllBytes(item.NombreFichero);
-                            item.NombreFichero = Path.GetFileName(item.NombreFichero);
-                        }
-                        LoadResult r = client.LoadJbs(item);
-                        resultadosEnvio.Add(r);
-                        Console.WriteLine(" Resultado: {0};", r.ResultadoCarga);
-                        _loger.Info("Envio: {0}, {1}. Resultado {2}", item.Expediente, item.NumeroFactura, r.ResultadoCarga);
-                        if (r.ResultadoCarga == ResultadoCarga.Erroneo)
-                        {
-                            foreach (var error in r.Errores)
-                            {
-                                _loger.Warn("Envio: {0}, {1}. Error {2} - {3}", item.Expediente, item.NumeroFactura, error.Nombre, error.Descripcion);
-                            }
-                        }
-                    }
+                    case JustificacionFileType.Bienes:
+                        ProcessJbs(clientArgs);
+                        break;
+                    case JustificacionFileType.Viajes:
+                        throw new NotImplementedException();
+                        break;
+                    case JustificacionFileType.Personal:
+                        throw new NotImplementedException();
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                        break;
                 }
             }
             catch (ArgException ex)
@@ -87,6 +49,63 @@ namespace ClienteJustificacion
             {
                 _loger.Error(e, "error!");
                 Console.WriteLine("Error." + e.Message);
+            }
+        }
+
+
+        static void ProcessJbs(ClientArgs clientArgs)
+        {
+            //Carga y parseo del fichero de datos jbs
+            var parseErrors = new List<ParseResult>(); //Listado de errores de parseo
+            var csv = new CsvReader(new StreamReader(clientArgs.File));
+            csv.Configuration.Delimiter = ";";
+            csv.Configuration.RegisterClassMap<JbsInterchageModelCsvDocMap>();
+            csv.Configuration.IgnoreReadingExceptions = true;
+            csv.Configuration.ReadingExceptionCallback = (ex, row) =>
+            {
+                int rownumber = row.Row;
+                string message = ex.Message + " " + ex.Data["CsvHelper"];
+                parseErrors.Add(new ParseResult() { Id = rownumber, Descripcion = message });
+            };
+            var records = csv.GetRecords<JbsInterchageModel>().ToList();
+
+            if (parseErrors.Count > 0)
+            {
+                Console.WriteLine("Se ha encontrado errores en el fichero de entrada, corrijalos antes de continuar.");
+                _loger.Error("Se ha encontrado errores en el fichero de entrada, corrijalos antes de continuar.");
+                foreach (var error in parseErrors)
+                {
+                    Console.WriteLine("Linea: {0}; Error {1};", error.Id, error.Descripcion);
+                    _loger.Error("Linea: {0}; Error {1};", error.Id, error.Descripcion);
+                }
+                return;
+            }
+
+            using (JustificationClient client = new JustificationClient())
+            {
+                client.ClientCredentials.UserName.UserName = clientArgs.User;
+                client.ClientCredentials.UserName.Password = clientArgs.Password;
+                var resultadosEnvio = new List<LoadResult>();
+                foreach (var item in records)
+                {
+                    Console.Write("Envio: {0}, {1}.", item.Expediente, item.NumeroFactura);
+                    if (!string.IsNullOrWhiteSpace(item.NombreFichero))
+                    {
+                        item.Fichero = File.ReadAllBytes(item.NombreFichero);
+                        item.NombreFichero = Path.GetFileName(item.NombreFichero);
+                    }
+                    LoadResult r = client.LoadJbs(item);
+                    resultadosEnvio.Add(r);
+                    Console.WriteLine(" Resultado: {0};", r.ResultadoCarga);
+                    _loger.Info("Envio: {0}, {1}. Resultado {2}", item.Expediente, item.NumeroFactura, r.ResultadoCarga);
+                    if (r.ResultadoCarga == ResultadoCarga.Erroneo)
+                    {
+                        foreach (var error in r.Errores)
+                        {
+                            _loger.Warn("Envio: {0}, {1}. Error {2} - {3}", item.Expediente, item.NumeroFactura, error.Nombre, error.Descripcion);
+                        }
+                    }
+                }
             }
         }
     }
